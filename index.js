dbf25c8d6da94381195e607cff439b3410b3edda
nomad.ry@gmail.com <nomad.ry@gmail.com>
2014-01-13 13:19:58 -0700

    replace globalKeyManager with keygen that saves to and checks localStorage

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ ndn.r = require('./lib/repo.js');
 ndn.io = require('./lib/ndn-io.js');
 ndn.utils = require('./lib/utils.js');
 ndn.rtc = require('./lib/ndn-rtc.js');
+ndn.globalKeyManager = require('./lib/ndn-keygen.js')
 ndn.d = require('./lib/ndn-d.js')
+
 module.exports = ndn;
--- test/offer.js
+++ test/offer.js
@@ -1,5 +1,4 @@
 window.ndn = require('../index.js');
 
-window.Face = ndn.d.newFace();
 
 console.log(window.transport)
